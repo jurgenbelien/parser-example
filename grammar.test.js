b37e4a0df50daf83834eebc785144f20a4bc7bc2
Jurgen Beliën <info@jurgenbelien.nl>
2021-01-26 17:26:34 +0100

    test: add tests

--- grammar.test.js
+++ grammar.test.js
@@ -2,6 +2,16 @@ const test = require('ava');
 const nearley = require('nearley');
 const grammar = require('./grammar');
 
-test('foo', t => {
-	t.pass();
-});
+const parser = new nearley.Parser(
+  nearley.Grammar.fromCompiled(grammar)
+);
+
+[
+	'1m',
+	'1h 1m',
+	'5h'
+].map(value => test(value, t => {
+	parser.feed(value);
+	parser.finish();
+	t.is(parser.results.length, 1);
+}));
